fcbcf4cd42493dc91e679e592f52cd9efdd88ca6
AngelQnc <88949903+AngelQnc@users.noreply.github.com>
2021-11-11 13:40:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+"use strict";
+var raiz = document.getElementById("raiz"),
+    salida = document.getElementById("salida");
+raiz.addEventListener("submit", proceso, false);
+function proceso(){
+    var valor = raiz["valor"].value,
+    resultado = [], res;
+    try{
+      valida(!isNaN(valor), "El valor debe ser un número.");
+      valida(valor > 0, "El valor no puede ser menor o igual a 0.");
+      res = Math.sqrt(valor);
+      resultado += "la raiz de " + valor + " = " + res + "\n";
+      raiz["salida"].value = resultado;
+    }
+    catch(e){
+      raiz["salida"].value = e.message;
+    }
+    
+  }
+raiz["limpiar"].addEventListener("click", limpiar, false);
+function limpiar(){
+  raiz["valor"].value = "";
+  raiz["salida"].value = "";
+}
 
-
+function valida(cond, mensaje) {
+  if (!cond) {
+    throw new Error(mensaje);
+  }
+}
